01365a187da5723f0bec40afb7f81a357cab722f
vukhuongduy3003 <vukhuongduy3003@gmail.com>
2023-02-22 08:34:38 +0700

    rename word

--- Lesson_2.3/src/main/java/com/vn/Bai3.java
+++ Lesson_2.3/src/main/java/com/vn/Bai3.java
@@ -64,4 +64,17 @@ public class Bai3 {
         } while (integerPositive <= 0);
         return integerPositive;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
+        }
+        return answer;
+    }
 }
--- Lesson_2.3/src/main/java/com/vn/Program.java
+++ Lesson_2.3/src/main/java/com/vn/Program.java
@@ -1,16 +1,38 @@
 package com.vn;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class Program
-{
-    public static void main( String[] args )
-    {
+public class Program {
+
+    public String longestCommonPrefix(String[] strs) {
+        String answer = "";
+        int m = 201;
+        for (String str : strs) {
+            m = Math.min(m, str.length());
+        }
+        int n = strs.length;
+        int i = 0;
+        while (i < m) {
+            for (int j = 0; j < n - 1; j++) {
+                if (strs[j].charAt(i) != strs[j + 1].charAt(i)) {
+                    return answer;
+                }
+            }
+            answer += strs[0].charAt(i);
+            i++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
 //        new Bai1();
 //        new Bai2();
 //        new Bai3();
-        new Bai4();
+//        new Bai4();
     }
 }
